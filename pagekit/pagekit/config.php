06407c3a3f844ef04a195c844070f003121e670d
Matteo Hertel <info@matteohertel.com>
2016-04-26 23:41:46 +0100

    Pagekit: I really like this CMS

---
+++ pagekit/pagekit/config.php
@@ -0,0 +1,25 @@
+<?php return array (
+  'application' => 
+  array (
+    'debug' => false,
+  ),
+  'database' => 
+  array (
+    'default' => 'mysql',
+    'connections' => 
+    array (
+      'mysql' => 
+      array (
+        'host' => 'database',
+        'user' => 'root',
+        'password' => 'SuperSecretPassword123',
+        'dbname' => 'laravel',
+        'prefix' => 'pk_',
+      ),
+    ),
+  ),
+  'system' => 
+  array (
+    'secret' => 'Qd56ZpK4ztx4iO3rYA8D1/eHN5yk8liXm3Pe5KRTHVDtxTctn8UN8nzq2ko6dHtc',
+  ),
+);
