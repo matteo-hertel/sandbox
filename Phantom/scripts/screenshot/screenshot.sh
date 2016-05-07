16e4d13482bde0534c324d2a4bbe8606ed4af4ae
Matteo Hertel <info@matteohertel.com>
2016-05-07 19:48:16 +0100

    Phantom.js: add some more resolutions to array

--- Phantom/scripts/screenshot/screenshot.sh
+++ Phantom/scripts/screenshot/screenshot.sh
@@ -6,9 +6,24 @@ if [ -z "$WEBSITE" ] ; then
 fi
 
 DIMENSIONS=(
+#Apple iPhone 6
 750px*1334px
+#Apple iPhone 5
 640px*1136px
-2550px*3096px
+#LG G4
+1440px*2560px
+#Samsung Galaxy Note 2
+720px*1280px
+#Microsoft Lumia 1020
+768px*1280px
+#HTC One
+1080px*1920px
+#Apple iPad Pro
+2048px*2732px
+#Apple iPad 3, 4, Air
+1536px*2048px
+#Samsung Galaxy Tab 3 10"
+800px*1280px
 );
 
 FILE=$WEBSITE
@@ -20,6 +35,6 @@ do
    :
     FILENAME=$(echo $i | sed "s/\*//")
 
-    phantomjs ./screenshot.js $WEBSITE ./output/$FILE/$FILENAME.png $i
+    phantomjs ./screenshot.js $WEBSITE ./output/phantom/$FILE/$FILENAME.png $i
     echo "$FILE-$FILENAME.png";
 done
