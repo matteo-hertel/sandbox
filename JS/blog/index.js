a60b80ba12785ef2e3cb3e33d267129f60d60a4b
Matteo Hertel <info@matteohertel.com>
2017-01-06 08:20:32 +0400

    recursively converting md to html

--- JS/blog/index.js
+++ JS/blog/index.js
@@ -1,14 +1,41 @@
 var Git = require("nodegit");
 var recursive = require('recursive-readdir');
 var fs = require('fs');
+var markdown = require( "markdown" ).markdown;
+var path = require("path");
+var mkpath = require('mkpath');
+function ignoreFunc(file, stats) {
+  // `file` is the absolute path to the file, and `stats` is an `fs.Stats`
+  // object returned from `fs.lstat()`.
+  console.log(path.extname(file));
+
+  return !stats.isDirectory() && path.extname(file) !== '.md';
+}
 
 function read(){
-    recursive('./tmp/up & going', function(err, files) {
+    recursive('./tmp/', [ignoreFunc], function(err, files) {
         // Files is an array of filename
         files.forEach((file, index) => {
+            console.log(file);
             fs.readFile(file, 'utf8', function(err, data) {
                 if (err) throw err;
-                console.log(data);
+                var html;
+                try {
+                    html = markdown.toHTML(data);
+                } catch (e) {
+                    html = "error";
+                } finally {
+
+                }
+                mkpath.sync(`./out/${path.dirname(file)}`.replace(/ /g, "-"), 0700);
+
+                fs.writeFile(`./out/${file}.html`.replace(/ /g, "-"),  html, function(err) {
+                    if(err) {
+                        return console.log(err);
+                    }
+
+                    console.log("The file was saved!");
+                });
             });
         });
     });
@@ -20,7 +47,7 @@ Git.Clone("https://github.com/getify/You-Dont-Know-JS.git", "./tmp")
     // Look up this known commit.
     .then(function(repo) {
         // Use a known commit sha from this repository.
-        read();
+        //read();
     })
     .catch(function(err) {
         console.log(err);
