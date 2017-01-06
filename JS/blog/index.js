9f3d95e14fe5dc084356785cdb6888db84d4fe6d
Matteo Hertel <info@matteohertel.com>
2017-01-06 07:30:30 +0400

    add git clone and recursive read

--- JS/blog/index.js
+++ JS/blog/index.js
@@ -1,15 +1,27 @@
 var Git = require("nodegit");
 var recursive = require('recursive-readdir');
+var fs = require('fs');
 
-
+function read(){
+    recursive('./tmp/up & going', function(err, files) {
+        // Files is an array of filename
+        files.forEach((file, index) => {
+            fs.readFile(file, 'utf8', function(err, data) {
+                if (err) throw err;
+                console.log(data);
+            });
+        });
+    });
+}
+read();
+return;
 // Clone a given repository into the `./tmp` folder.
 Git.Clone("https://github.com/getify/You-Dont-Know-JS.git", "./tmp")
-  // Look up this known commit.
-  .then(function(repo) {
-    // Use a known commit sha from this repository.
-    recursive('./tmp', function (err, files) {
-      // Files is an array of filename
-      console.log(files);
+    // Look up this known commit.
+    .then(function(repo) {
+        // Use a known commit sha from this repository.
+        read();
+    })
+    .catch(function(err) {
+        console.log(err);
     });
-  })
-  .catch(function(err) { console.log(err); });
