c7aa8ce2fb46a1f86095388c906b4cb405ae6333
Matteo Hertel <info@matteohertel.com>
2017-01-14 16:50:13 +0000

    add creste handler

---
+++ Serverless/test/serverless-rest-api/todos/create.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const AWS = require("aws-sdk");
+const dynamoDb = new AWS.DynamoDb.DocumentClient();
+const uuid = require("uuid");
+
+module.exports.create = (event, context, callback) => {
+    const timestamp = new Date().getTime();
+    const data = JSON.parse(event.body);
+
+    if (typeof data.text !== "string") {
+        console.log("Validation failed");
+        callback(new Error(`Can't create todo item, validation failed`));
+    }
+
+    const params = {
+        TableName: "todos",
+        Item: {
+            id: uuid.v1(),
+            text: data.text,
+            checked: false,
+            createdAt: timestamp,
+            updatedAt: timestamp
+        }
+    };
+    dynamoDb.put(params, (error, result) => {
+        if (error) {
+            console.log(error);
+            callback(new Error(`Can't create todo item, failed to write`));
+            return;
+        }
+
+        const response = {
+            statusCode: 200,
+            body: JSON.stringify(result.Item)
+        };
+        callback(null, response);
+    });
+};
