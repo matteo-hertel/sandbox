86840be4f1d286de23f5a71d481655877a031d38
Matteo Hertel <info@matteohertel.com>
2017-01-27 17:35:34 +0400

    refactor tests

--- JS/blog/__tests__/git/git.js
+++ JS/blog/__tests__/git/git.js
@@ -2,10 +2,10 @@ require('dotenv').config({
     path: `${__dirname}/../../.env`
 });
 const git = require(`${__dirname}/../../modules/git`);
-describe("Git Testing", () => {
-it('should return the file when calling github api for a particular file', () => {
-    return git.getFile("matteo-hertel", "blog", "master", "README.md").then((file) => {
-        expect(file).not.toBeFalsy();
+describe("Git Module", () => {
+    it('should return the file when calling github api for a particular file', () => {
+        return git.getFile("matteo-hertel", "blog", "master", "README.md").then((file) => {
+            expect(file).not.toBeFalsy();
+        });
     });
-});
 });
