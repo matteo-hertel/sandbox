318ef5a044bcbe2c9b3ef0d598830b8c4bdb00c0
Matteo Hertel <info@matteohertel.com>
2017-01-28 11:41:22 +0400

    add more tests

--- JS/blog/__tests__/git/git.test.js
+++ JS/blog/__tests__/git/git.test.js
@@ -6,14 +6,12 @@ jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
 const git = require(`${__dirname}/../../modules/git`);
 describe("Git Module", () => {
     it('should return the file when calling github api for a particular file', () => {
-        jest.resetModules();
         return git.getFile("matteo-hertel", "blog", "master", "README.md").then((file) => {
             expect(file).not.toBeFalsy();
         });
     });
 
     it('should throw an error if an error occurres while getting a file', () => {
-        jest.resetModules();
         git.getFile("matteo-hertel", "blog", "does-not-exists-file", "README.md")
             .catch((exc) => {
                 expect(exc).toBeDefined();
@@ -21,7 +19,6 @@ describe("Git Module", () => {
     });
 
     it('should return an array of files if a folder path is passed', () => {
-        jest.resetModules();
         return git.getFolder("matteo-hertel", "blog", "master", "posts")
             .then((files) => {
                 expect(files.length).toBeTruthy();
@@ -29,7 +26,6 @@ describe("Git Module", () => {
     });
 
     it('should throw an error if an error occurres while getting a folder', () => {
-        jest.resetModules();
         git.getFolder("matteo-hertel", "blog", "does-not-exists-folder", "error")
         .catch((exc) => {
             expect(exc).toBeDefined();
--- JS/blog/__tests__/markdown/markdown.test.js
+++ JS/blog/__tests__/markdown/markdown.test.js
@@ -1,15 +1,31 @@
-test('markdown input should return HTML', () => {
-    const markdownProcessor = require(`${__dirname}/../../modules/markDownProcessor`);
-    let markdown = `# H1
+const markdownProcessor = require(`${__dirname}/../../modules/markDownProcessor`);
+describe("Markdown Module", () => {
+    it('should return HTML from markdown input', () => {
+        let markdown = `# H1
 ## H2
 ### H3`;
-    let html = `<h1>H1</h1>
+        let html = `<h1>H1</h1>
 
 <h2>H2</h2>
 
 <h3>H3</h3>`;
-    markdownProcessor.convert(markdown, html).then((converted) => {
-        expect(converted).toBe(html);
-    });
+        markdownProcessor.convert(markdown)
+        .then((converted) => {
+            expect(converted).toBe(html);
+        });
 
+    });
+    it('should invoke catch on error', () => {
+        let markdown = ``;
+        let html = `<h1>H1</h1>
+
+<h2>H2</h2>
+
+<h3>H3</h3>`;
+        markdownProcessor.convert(markdown)
+        .catch((exc) => {
+            expect(exc).toBeDefined();
+        });
+
+    });
 });
