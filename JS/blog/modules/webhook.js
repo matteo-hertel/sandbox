152e71a7332c18139e3adc225be1ecb88e3d4dde
Matteo Hertel <info@matteohertel.com>
2017-02-15 01:17:10 +0400

    add get repo name

--- JS/blog/modules/webhook.js
+++ JS/blog/modules/webhook.js
@@ -14,6 +14,23 @@ const acceptValidPRs = (payload) => {
         return resolve(payload.pull_request);
     });
 };
+const getRepoAndOwner = (payload) => {
+
+    return new Promise((resolve, reject)=>{
+        try{
+            var out = {
+                repo: "",
+                owner: ""
+            };
+            var repoName = payload.repository.full_name.split("/");
+            out.repo = repoName[0];
+            out.owner = repoName[1];
+            resolve(out);
+        }catch(exc){
+            reject("Cannot extract repo owner and name");
+        }
+    });
+};
 
 module.exports = {
     acceptValidPRs
