c81dc202dce7e6126d0d848063396a31c7445733
Matteo Hertel <info@matteohertel.com>
2018-10-27 17:31:11 +0100

    add simple http server

--- GoMod/main.go
+++ GoMod/main.go
@@ -1,9 +1,14 @@
 package main
 
-import (
-	"fmt"
-)
+import "github.com/kataras/iris"
 
 func main() {
-	fmt.Print("Hello There")
+	app := iris.Default()
+	app.Get("/ping", func(ctx iris.Context) {
+		ctx.JSON(iris.Map{
+			"message": "pong",
+		})
+	})
+	// listen and serve on http://0.0.0.0:8080.
+	app.Run(iris.Addr(":8080"))
 }
