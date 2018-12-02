444d5b5510a6f22a902867644c7992e79ceb0861
Matteo Hertel <info@matteohertel.com>
2018-12-02 13:32:12 +0000

    remove omi

--- omi/src/index.js
+++
@@ -1,7 +0,0 @@
-import { render } from 'omi'
-import './elements/animated-quotes'
-//import registerServiceWorker from './assets/register-service-worker'
-//import store from './store/app-store'
-
-render(<animated-quotes author="papoi" />, '#root')
-//registerServiceWorker()
