7ace8a596be6cafef4cd64e0b967f4fcc50edadb
Matteo Hertel <info@matteohertel.com>
2020-07-19 19:17:19 +0100

    rust: add variable and mutating variable example

--- RustCrashCourse/say-my-name/src/main.rs
+++ RustCrashCourse/say-my-name/src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    let name = "Matt";
+    let mut mutable_name = "Bob";
+
+    println!("Hello, {}", name);
+    println!("Hello mutable, {}", mutable_name);
+    mutable_name = "Elon";
+    println!("Hello mutated, {}", mutable_name);
 }
