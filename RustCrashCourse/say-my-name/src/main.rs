cd150a7a3c300f00bdaf7c7e5a87eef62dd0a9bc
Matteo Hertel <info@matteohertel.com>
2020-07-20 08:52:02 +0100

    rust: moving old examples to other files

--- RustCrashCourse/say-my-name/src/main.rs
+++
@@ -1,9 +0,0 @@
-use std::io;
-
-fn main() {
-    println!("Please enter your name: ");
-    let mut name = String::new();
-
-    io::stdin().read_line(&mut name);
-    println!("Hello, {}", name);
-}
