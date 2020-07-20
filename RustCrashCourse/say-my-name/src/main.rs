362318479f1ed3bebd92208c8bb868495c9ba124
Matteo Hertel <info@matteohertel.com>
2020-07-20 09:40:46 +0100

    rust: DRY up code

--- RustCrashCourse/say-my-name/src/main.rs
+++ RustCrashCourse/say-my-name/src/main.rs
@@ -5,45 +5,33 @@ fn _test() {
     println!("HI")
 }
 fn main() {
-    let _test: u32 = 0;
     println!("Please enter number no. 1: ");
-
-    let mut first_string = String::new();
-    io::stdin().read_line(&mut first_string).unwrap();
-
-    let a: u32;
-
-    match first_string.trim().parse() {
-        Ok(val) => {
-            a = val;
-        }
-        Err(err) => {
-            println!("Please enter a valid number, {}", err);
-            process::exit(1);
-        }
-    }
-
+    let a = read_user_input();
     println!("Please enter number no. 2: ");
-
-    let mut second_string = String::new();
-    io::stdin().read_line(&mut second_string).unwrap();
-
-    let b: u32;
-
-    match second_string.trim().parse() {
-        Ok(val) => {
-            b = val;
-        }
-        Err(err) => {
-            println!("Please enter a valid number, {}", err);
-            process::exit(1);
-        }
-    }
+    let b = read_user_input();
 
     let result = sum(a, b);
-    println!("{} + {} = {}", first_string, second_string, result);
+    println!("{} + {} = {}", a, b, result);
 }
 
 fn sum(a: u32, b: u32) -> u32 {
-    return a + b;
+    a + b
+}
+
+fn read_user_input() -> u32 {
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+
+    let digit: u32;
+
+    match input.trim().parse() {
+        Ok(val) => {
+            digit = val;
+        }
+        Err(err) => {
+            println!("Please enter a valid number, {}", err);
+            process::exit(1);
+        }
+    }
+    digit
 }
