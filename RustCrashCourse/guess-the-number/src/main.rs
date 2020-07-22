3d5be9093011d0ccb83c78e5aa696304df0f9e16
Matteo Hertel <info@matteohertel.com>
2020-07-22 22:39:01 +0100

    rust: use shadowing variables and return from match

--- RustCrashCourse/guess-the-number/src/main.rs
+++ RustCrashCourse/guess-the-number/src/main.rs
@@ -5,23 +5,20 @@ use std::process;
 
 fn main() {
     let secret_number = rand::thread_rng().gen_range(1, 101);
-    println!("{}", secret_number);
 
     loop {
-        let mut guess_string = String::new();
+        let mut guess = String::new();
         io::stdin()
-            .read_line(&mut guess_string)
+            .read_line(&mut guess)
             .expect("failed to read line");
 
-        let guess: u32;
-
-        match guess_string.trim().parse() {
-            Ok(val) => guess = val,
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
             Err(err) => {
                 println!("Please enter a valid number, {}", err);
                 process::exit(1);
             }
-        }
+        };
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too low"),
             Ordering::Greater => println!("Too big"),
