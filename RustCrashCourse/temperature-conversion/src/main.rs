1425106138e2f63afa27bc802297edc5217546f8
Matteo Hertel <info@matteohertel.com>
2020-07-25 11:44:55 +0100

    rust: more comprehensive temperature converter

--- RustCrashCourse/temperature-conversion/src/main.rs
+++ RustCrashCourse/temperature-conversion/src/main.rs
@@ -2,27 +2,61 @@ use std::io;
 use std::process;
 
 fn main() {
-    println!("Type the temperature in fahrenheit you want to convert");
+    println!(
+        "What would you like to convert, C to F or F or C (type c for C to F, or f for F to C)"
+    );
+    let mut temperature = String::new();
 
-    let mut fahrenheit = String::new();
     io::stdin()
-        .read_line(&mut fahrenheit)
+        .read_line(&mut temperature)
+        .expect("failed to read line for temperature");
+
+    match temperature.trim() {
+        "c" | "C" => use_celsius_to_fahrenheit(),
+        "f" | "F" => use_fahrenheit_to_celsius(),
+        _ => {
+            println!("You need to chose between f and c");
+            process::exit(1);
+        }
+    };
+}
+fn ask_for_temperature() -> f64 {
+    println!("Type the temperature you want to convert");
+
+    let mut degrees = String::new();
+    io::stdin()
+        .read_line(&mut degrees)
         .expect("failed to read line");
 
-    let fahrenheit: f64 = match fahrenheit.trim().parse() {
+    return match degrees.trim().parse() {
         Ok(f) => f,
         Err(_) => {
             println!("You need to pass a valid number");
             process::exit(1);
         }
     };
+}
+fn use_fahrenheit_to_celsius() {
+    let f = ask_for_temperature();
     println!(
-        "{} fahrenheit equal {} celsius",
-        fahrenheit,
-        fahrenheit_to_celsius(fahrenheit)
+        "{} degrees fahrenheit equal {} celsius",
+        f,
+        fahrenheit_to_celsius(f)
+    );
+}
+fn use_celsius_to_fahrenheit() {
+    let c = ask_for_temperature();
+    println!(
+        "{} degrees celsius equal {} fahrenheit",
+        c,
+        celsius_to_fahrenheit(c)
     );
 }
 
 fn fahrenheit_to_celsius(f: f64) -> f64 {
     (f - 32.0) * 5.0 / 9.0
+}
+
+fn celsius_to_fahrenheit(c: f64) -> f64 {
+    (c * 9.0 / 5.0) + 32.0
 }
